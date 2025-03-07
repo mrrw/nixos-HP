b98d65dd42c6a67780a4d9442236798edbeab1c4
mrrw <michael.milk@zoho.com>
2025-03-06 21:20:05 -0500

    created git-push.bash, which has errors.

---
+++ home/mrrw/bin/git-push.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+#set -x
+
+# By mrrw, @2025
+f='/home /etc'
+
+# To update the root repository, enter the following commands:
+sudo cd /
+sudo git add $f
+sudo git commit -a
+sudo git push -u origin main
+
+#This repository tracks my first foray into nixos.  This is my personal backup (in case I REALLY donk things up and can't rollback somehow).  It's also public, for ease of access to myself and anyone interested in peeking at my code.
