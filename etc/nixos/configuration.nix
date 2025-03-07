3eabe39235f14075d44f84a1a7ed22c5f30a43a9
mrrw <michael.milk@zoho.com>
2025-03-07 15:35:02 -0500

    Still working on fixing sound.

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -126,7 +126,7 @@
 # You may need to add users to the audio group for them to be able to use audio devices:
 #users.extraUsers.mrrw.extraGroups = [ "audio" ... ];
 
-sound.enable = true ;
-hardware.pulseaudio.enable = false ;
+#sound.enable = true ; ## nixos no longer supports "sound"
+# hardware.pulseaudio.enable = true ;  ## a puzzle piece, but fails by itself.
 # Disable pulseaudio if you want to use ALSA directly.
 }
