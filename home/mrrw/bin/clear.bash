9667427abda8998a494717ef038b1c0259f470bc
mrrw <michael.milk@zoho.com>
2025-03-08 12:06:13 -0500

    adding additional pl functionalities

---
+++ home/mrrw/bin/clear.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ~/bin/clear.bash
+# Clear and print useful information into tmux panes.
+# ...by mrrw
+
+clear
+. ~/.alias
+alias git-push
+alias nixos-mrrw
+echo ''
+if [[ $TMUX_PANE == %0 ]] ; then
+	ls -A --color=auto --group-directories-first ~ ;
+ 	ls -A --color=auto --group-directories-first ~/* ;
+elif [[ $TMUX_PANE == %1 ]] ; then
+	neofetch ;
+elif [[ $TMUX_PANE == %2 ]] ; then
+	bash $HOME/bin/pl.bash -t ;
+fi
+
--- home/mrrw/bin/pl.bash
+++ home/mrrw/bin/pl.bash
@@ -1,20 +1,50 @@
 #!/bin/bash
-#
-# This file can be found at:
 # ~/bin/pl.bash
-#
-# This simple bash script is meant to help you do the following:
 # Archive your personal feelings, ideas, and progress into a simple log.
-#
+# Use options to manipulate or print personal log data.
+# 
+# INITIAL COMMANDS:
+	d=$HOME/lib/txt/pl-$USER && [ ! -d $d ] && mkdir -p $d
+	f="$d/pl-$(date +%Y%m%d).txt"
+	var=$HOME/var/ && [ ! -d $var ] && mkdir -p $d
+	ftmp=$var/pl-tmp.txt
 
-# Set some variables:
-
-d=$HOME/lib/txt/pl-$USER
-f=$d/pl_$(date +%Y%m%d).txt
+# GET OPTIONS:
+while getopts ":aptxy" opt; do
+	case ${opt} in
+		a) #append
+			[ ! -e $f ] && touch $f ] && echo -e "Creating $f." ;
+			tail -3 $f
+			echo "Appending standard input to todays file.  Press ctr-d when done." ;
+			cat - >> $f ;
+			exit ;;
+		t) # print last few lines after last empty line
+			shift
+			sed ':a;/$/{N;s/.*\n\n//;ba;}' $f
+			exit ;;
+		x) # debug
+			set x;
+			shift;
+			;;
+		y) # call yesterdays pl instead of todays
+			PS3="Choose a file from up to seven days ago."
+			select f in $(ls $d | tail -n 8 | head -n 7 | sort -r); do
+				sed ':a;/$/{N;s/.*\n\n//;ba;}' $d/$f
+				break
+			done
+			exit ;;
+		*)
+			echo "Invalid option: -${OPTARG}."
+			exit 1 ;;	
+	esac
+done
+		
 
 # Execute code:
-
-if [ ! -d $d ] ; then mkdir -p $d ; fi
+# EXPERIMENTAL:  TAKE STANDARD INPUT AND APPEND TO $f
+# while IFS="\n" read -r line ; do
+#   echo "${line}" >> $f
+# done
 
 if [ ! -s $f ] ; then
 	echo -e "Creating $f." ;
