a0d47ce4130d43a59de56984e25d98ab1ed0bda5
mrrw <michael.milk@zoho.com>
2025-03-08 12:35:30 -0500

    tweeked clear.bash ls commands

--- home/mrrw/bin/clear.bash
+++ home/mrrw/bin/clear.bash
@@ -8,12 +8,13 @@ clear
 alias git-push
 alias nixos-mrrw
 echo ''
+
 if [[ $TMUX_PANE == %0 ]] ; then
-	ls -A --color=auto --group-directories-first ~ ;
- 	ls -A --color=auto --group-directories-first ~/* ;
+	cd ~ ;
+	ls -A --color=auto --group-directories-first . ;
+ 	ls -A --color=auto --group-directories-first ./* ;
 elif [[ $TMUX_PANE == %1 ]] ; then
 	neofetch ;
 elif [[ $TMUX_PANE == %2 ]] ; then
 	bash $HOME/bin/pl.bash -t ;
 fi
-
